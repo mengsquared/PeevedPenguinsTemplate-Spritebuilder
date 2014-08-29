8fd5efbcc08cfea9a2cab18fe686b4add47c6ea0
mengsquared <mengmengjie@0587373822.wireless.umich.net>
2014-08-28 22:32:12 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -0,0 +1,23 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Mengmeng Jie on 8/28/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Seal created");
+    }
+    
+    return self;
+}
+
+@end
