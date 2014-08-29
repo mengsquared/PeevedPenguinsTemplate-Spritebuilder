fe3705b1b80a57b984f2bb389b5b2081335223c4
mengsquared <mengmengjie@0587373822.wireless.umich.net>
2014-08-28 23:38:23 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/MainScene.m
+++ PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -11,12 +11,10 @@
 @implementation MainScene
 
 - (void)play {
-    CCLOG(@"play button pressed");
-}
-
-- (void)play {
     CCScene *gameplayScene = [CCBReader loadAsScene:@"Gameplay"];
     [[CCDirector sharedDirector] replaceScene:gameplayScene];
 }
+
+
 
 @end
